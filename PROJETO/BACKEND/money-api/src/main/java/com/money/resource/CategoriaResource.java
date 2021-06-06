00b32506202d561462338dedf27fb174e513c7bc
Luiz Eduardo <43974104+LuizOMartins@users.noreply.github.com>
2021-06-06 18:49:54 -0300

    GET categorias

---
+++ PROJETO/BACKEND/money-api/src/main/java/com/money/resource/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.money.resource;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.money.model.Categoria;
+import com.money.repository.CategoriaRepository;
+
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+
+}
