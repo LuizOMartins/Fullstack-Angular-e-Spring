07662462d983588c047a21dc2abe5401e7dbf19f
Luiz Eduardo <43974104+LuizOMartins@users.noreply.github.com>
2021-08-05 12:55:02 -0300

    Desafio 4040 categoria não encontrada

--- PROJETO/BACKEND/money-api/src/main/java/com/money/resource/CategoriaResource.java
+++ PROJETO/BACKEND/money-api/src/main/java/com/money/resource/CategoriaResource.java
@@ -38,8 +38,9 @@ public class CategoriaResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findById(codigo);
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		 Optional<Categoria> categoria = this.categoriaRepository.findById(codigo);
+		 return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
 	}
 	
 	@GetMapping
